52a3e84134687da9c2c5accf18c6f1f0ff7b190d
Gabriel P. B. Piazza <pedro.piazza99@gmail.com>
2023-05-23 08:04:01 -0300

    feat: add base strcture of customer model

---
+++ src/domain/models/customer.go
@@ -0,0 +1,16 @@
+package models
+
+import "github.com/google/uuid"
+
+type CustomerRole string
+
+const (
+	COMPANY_CR CustomerRole = "COMPANY"
+	PERSON_CR  CustomerRole = "PERSON"
+)
+
+type Customer struct {
+	ID    uuid.UUID
+	TaxID string
+	Name  string
+}
