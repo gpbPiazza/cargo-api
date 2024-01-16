ca38cf50abd6ef2de397ce4e68cd0efa0ba7988b
Gabriel P. B. Piazza <pedro.piazza99@gmail.com>
2024-01-16 09:31:58 -0300

    temp: save changes

--- src/infrastructure/repositories/customer_repository.go
+++ src/infrastructure/repositories/customer_repository.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/google/uuid"
+	"github.com/gpbPiazza/cargo-api/src/infrastructure/database"
 )
 
 type CustomerRepository interface {
@@ -20,7 +21,18 @@ func NewCustomerRepository() CustomerRepository {
 }
 
 func (cr *customerRepository) FindByTaxID(ctx context.Context, taxID string) {
+	conn := database.ConnFromCtx(ctx)
+	defer conn.Close()
 
+	row := conn.QueryRowContext(ctx, "SELECT * FROM customers where taxID = %s", taxID)
+	if row.Err() != nil {
+		// TODO:
+	}
+
+	// TODO: how to make repository not depende from domain layer to scan data model?
+	// should i create a DB Model where a aways return the DB model and in domain layer i translate
+	// db model to domain Model, using the factories? i will read more about it
+	// return row.Scan(&models.Customer{})
 }
 
 func (cr *customerRepository) FindByID(ctx context.Context, ID uuid.UUID) {
