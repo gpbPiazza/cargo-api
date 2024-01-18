f05e75614615e577cd8ba44b2ac02894a9a99ec9
Gabriel P. B. Piazza <pedro.piazza99@gmail.com>
2024-01-18 11:27:46 -0300

    fix: grammar error

--- src/application/sign_up_app.go
+++ src/application/sign_up_app.go
@@ -7,19 +7,19 @@ import (
 	"github.com/gpbPiazza/cargo-api/src/infrastructure/validator"
 )
 
-type SignUpApp struct {
+type SignupApp struct {
 }
 
-type SignUpParams struct {
+type SignupParams struct {
 	TaxID    string              `json:"tax_id" validate:"required,validate-tax-id"`
 	Name     string              `json:"name" validate:"required"`
 	Role     models.CustomerRole `json:"role" validate:"required,oneof=SHIPPER RECEIVER"`
 	Phone    string              `json:"phone" validate:"required"`
 	Email    string              `json:"email" validate:"required,email"`
-	Password string              `json:"password" validate:"required,lte=72,gte=8"` // TODO: IMPROVE PASSWORD VALIDATION
+	Password string              `json:"password" validate:"required,lte=72,gte=8"` // TODO: IMPROVE PASSWORD VALIDATION require especial characters, min numbers, min capital letters
 }
 
-func (sc *SignUpApp) SignUp(ctx context.Context, customerParams SignUpParams) error {
+func (sc *SignupApp) Signup(ctx context.Context, customerParams SignupParams) error {
 	// TODO: call sanitizer.SanitizeInput
 
 	if err := validator.Validate(ctx, customerParams, validator.ValidateTaxID); err != nil {
