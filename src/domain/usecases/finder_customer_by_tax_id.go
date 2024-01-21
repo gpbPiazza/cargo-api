a116ab61bce4d737c69e522ccfbf114b423e9eb0
Gabriel P. B. Piazza <pedro.piazza99@gmail.com>
2024-01-21 11:01:48 -0300

    refactor: change file name and interface file to infinitive

--- src/domain/usecases/finder_customer_by_tax_id.go
+++ src/domain/usecases/finder_customer_by_tax_id.go
@@ -6,6 +6,6 @@ import (
 	"github.com/gpbPiazza/cargo-api/src/domain/models"
 )
 
-type FindCustomerRepository interface {
+type FinderCustomerRepository interface {
 	FindByTaxID(ctx context.Context, taxID string) (models.Customer, error)
 }
