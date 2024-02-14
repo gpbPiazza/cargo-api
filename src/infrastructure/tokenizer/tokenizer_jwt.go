f37d0b95dcd420e1b23e26cf53ee9271df7852a9
Gabriel P. B. Piazza <pedro.piazza99@gmail.com>
2024-02-14 11:20:34 -0300

    refactor: change file name

--- src/infrastructure/tokenizer/tokenizer_jwt.go
+++ src/infrastructure/tokenizer/tokenizer_jwt.go
@@ -1,4 +1,4 @@
-package jwt
+package tokenizer
 
 import (
 	"time"
@@ -8,18 +8,18 @@ import (
 	"github.com/gpbPiazza/cargo-api/src/infrastructure/envs"
 )
 
-type jwtService struct {
+type tokenizerService struct {
 	signedKey []byte
 }
 
-func NewJwtService() *jwtService {
+func NewService() *tokenizerService {
 	accessEnv := envs.New().AccessEnv
-	return &jwtService{
+	return &tokenizerService{
 		signedKey: []byte(accessEnv.SignedKey),
 	}
 }
 
-func (js *jwtService) Token(userID uuid.UUID, expirationAccessTime float64) (string, error) {
+func (js *tokenizerService) Token(userID uuid.UUID, expirationAccessTime float64) (string, error) {
 	accessExpiration := time.Now().Add(time.Duration(expirationAccessTime) * time.Second).Unix()
 
 	claims := jwt.MapClaims{
