c88fe3211911fd85797bbc70c4692fdfa098d862
Gabriel P. B. Piazza <pedro.piazza99@gmail.com>
2024-02-15 09:25:25 -0300

    feat: config all http_api package to works with many route groups and api start

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,8 +1,19 @@
 package main
 
-import "github.com/gpbPiazza/cargo-api/src/presentation/http_api"
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/gpbPiazza/cargo-api/src/infrastructure/databases/sql"
+	"github.com/gpbPiazza/cargo-api/src/infrastructure/envs"
+	"github.com/gpbPiazza/cargo-api/src/presentation/http_api"
+)
 
 func main() {
+	ctx := context.Background()
+	envs := envs.New()
+	sql.ConnectDB(ctx)
 	app := http_api.NewApp()
-	app.Start()
+	log.Fatal(app.Listen(fmt.Sprintf(":%s", envs.ServerPort)))
 }
